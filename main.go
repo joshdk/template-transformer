e09803b051719dfce6c1ff08c9541f46bfc05622
Josh Komoroske <jdkomo@gmail.com>
2022-06-24 23:33:40 -0700

    feat: resolve property values from environment

--- main.go
+++ main.go
@@ -38,5 +38,41 @@ func mainCmd() error {
 	// For now, just log the plugin configuration.
 	log.Printf("plugin config: %+v", cfg)
 
+	// Resolve all property values and build a complete map of property names
+	// to their respective values.
+	properties := make(map[string]string)
+	for _, property := range cfg.Properties {
+		value, found := resolve(property)
+		if !found {
+			return fmt.Errorf("could not resolve value for property %s", property.Name)
+		}
+
+		properties[property.Name] = value
+	}
+
+	// For now, just log the property values.
+	log.Printf("properties: %+v", properties)
+
 	return nil
+}
+
+// resolve obtains a final value for the given property.
+func resolve(property config.Property) (string, bool) {
+	// Check every environment variable, in the order they were specified, and
+	// return the first value that is found.
+	for _, source := range property.Source {
+		// If this environment variable contains a value then return it.
+		if value := os.Getenv(source); value != "" {
+			return value, true
+		}
+	}
+
+	// No environment variables contained a value. If a default is configured
+	// then return it.
+	if property.Default != "" {
+		return property.Default, true
+	}
+
+	// No value could be obtained.
+	return "", false
 }
